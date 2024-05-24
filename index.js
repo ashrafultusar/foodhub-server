1537f245ffc2c86e4282d39c21c112518f802aa2
ashrafultusar <ashrafulislamtusar@gmail.com>
2024-05-24 19:59:24 +0600

    added all update

--- index.js
+++ index.js
@@ -133,6 +133,15 @@ next()
       res.send(result);
     });
 
+    app.post('/menu',verifyToken,verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item)
+      res.send(result)
+
+    })
+    
+
+
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollection.find().toArray();
       res.send(result);
